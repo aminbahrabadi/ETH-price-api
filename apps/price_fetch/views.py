ef5f8362044d65d53e73634e604cbb17cbbfc462
Amin Bahrabadi <amin.bfamily@gmail.com>
2024-05-10 08:36:44 +0330

    Add requirements

--- apps/price_fetch/views.py
+++ apps/price_fetch/views.py
@@ -8,7 +8,6 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from web3 import Web3
 
 from apps.base.rate_limits import GeneralThrottle
 from apps.price_fetch.models import Price
